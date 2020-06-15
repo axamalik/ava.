9f40a5b7be8793844dd0c107240f4ac562a6a752
axamalik <66937106+axamalik@users.noreply.github.com>
2020-06-15 11:40:26 +0500

    Create ava.py

---
+++ ava.py
@@ -0,0 +1,8 @@
+n=int(input("maths marks:"))
+print(f'you enter marks {n}')
+g=int(input("physics marks:"))
+print(f'you enter marks {g}')
+u=int(input("urdu marks"))
+print(f'you enter marks {u}')
+average_of_student = (n+g+u)/3
+print(average_of_student)
